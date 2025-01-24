961170329c4d89589647081e7ff7cb46617c8462
m1maker <georgijbondarenko248@gmail.com>
2025-01-25 01:02:22 +0300

    Try to implement commands reading.

--- addon/globalPlugins/NVDAControlEx/__init__.py
+++ addon/globalPlugins/NVDAControlEx/__init__.py
@@ -1,6 +1,8 @@
 import globalPluginHandler
 import speech
 import ctypes
+from threading import Thread
+
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	pipeCommands = {
@@ -28,7 +30,47 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		if self.h_pipe == ctypes.windll.kernel32.INVALID_HANDLE_VALUE:
 			return None
 
+		Thread t(target = self.read_task)
+		t.start()
+
 	def __del__(self):
 		if self.h_pipe:
 			ctypes.windll.kernel32.CloseHandle(self.h_pipe)
 			self.h_pipe = None
+
+	def read_task(self):
+		buffer_size = 1024
+		while True:
+			result = ctypes.windll.kernel32.ConnectNamedPipe(self.h_pipe, None)
+			if result == 0:
+				break
+
+			try:
+				while True:
+					buffer = ctypes.create_string_buffer(buffer_size)
+
+					bytes_read = ctypes.c_ulong(0)
+					read_result = ctypes.windll.kernel32.ReadFile(
+						self.h_pipe,
+						buffer,
+						buffer_size,
+						ctypes.byref(bytes_read),
+						None
+					)
+
+					if read_result == 0 or bytes_read.value == 0:
+						break
+
+					received_data = buffer.value.decode('utf-8')
+
+					command_parts = received_data.split(' ', 1)
+					command = command_parts[0]
+					if command in self.pipeCommands:
+						args = command_parts[1] if len(command_parts) > 1 else ""
+						self.pipeCommands[command](args)
+
+					if command == "exit":
+						break
+
+			finally:
+				ctypes.windll.kernel32.DisconnectNamedPipe(self.h_pipe)
